a7fdaefb0102c7b937e487729bbcbd3573f17833
Ashenafi Yakob <asheimade@gmail.com>
2024-01-04 09:49:41 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [userName, setUserName] = useState('')
+  
+  
+  useEffect(() => {
+    getNames();
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUserName(response.data);
+  }
+  
+  return (
+    <>
+      <h1>My frontend</h1>    
+      <h3>My Name is {userName}</h3>
+    </>
+  );
+}
+
+export default App;
